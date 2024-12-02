d81018a2917eb843f038d3895a0c124337a23077
suhyeok0422 <chltngur133@naver.com>
2024-12-02 10:02:11 +0900

    Create 2444.c

---
+++ 2444.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0;j < n - i;j++) {
+			printf(" ");
+		}
+		for (int k = 0;k < (i * 2) - 1;k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = 0;j < n - i;j++) {
+			printf(" ");
+		}
+		for (int k = 0;k < (i * 2) - 1;k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
