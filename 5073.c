77fa5218827d5e8384701762a66a419ba19dbbda
suhyeok0422 <chltngur133@naver.com>
2024-12-04 13:48:46 +0900

    Update 5073.c

--- 5073.c
+++ 5073.c
@@ -2,9 +2,9 @@
 
 int main() 
 {
-  int a, b, c;
+  	int a, b, c;
 	while (1) 
-    {
+    	{
 		scanf("%d %d %d", &a, &b, &c);
 
 		if (a == 0 && b == 0 && c == 0)
@@ -14,7 +14,7 @@ int main()
 			printf("Invalid\n");
         
 		else if (a == b || b == c || a == c) 
-        {
+        	{
 			if (a == b && b == c && c == a)
 				printf("Equilateral\n");
 			else
