47e84f1866da0299fa349cf89716cdf6131a6980
suhyeok0422 <chltngur133@naver.com>
2024-12-09 09:28:40 +0900

    Update 20006.c

--- 20006.c
+++ 20006.c
@@ -52,9 +52,12 @@ int main() {
    }
 
    for (i = 0; i < team_cnt; i++) {
-      if (team_idx[i] == team_num) printf("Started!\n");
-      else printf("Waiting!\n");
-      for (j = 0; j < team_idx[i]; j++)printf("%d %s\n", int_team[i][j], char_team[i][j]);
+      if (team_idx[i] == team_num) 
+         printf("Started!\n");
+      else 
+         printf("Waiting!\n");
+      for (j = 0; j < team_idx[i]; j++)
+         printf("%d %s\n", int_team[i][j], char_team[i][j]);
    }
    return 0;
 }
